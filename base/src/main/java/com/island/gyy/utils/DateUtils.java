5f81f2be72bf517771cbe3cb20ca1143dc5e2eea
YIslandG <935627337@qq.com>
2016-10-08 16:16:05 +0800

    1.添加本月上月数据格式

--- base/src/main/java/com/island/gyy/utils/DateUtils.java
+++ base/src/main/java/com/island/gyy/utils/DateUtils.java
@@ -1,6 +1,15 @@
 package com.island.gyy.utils;
 
+
+
+
+
+import android.text.format.DateFormat;
+
+import com.island.gyy.base.BaseApplication;
+
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -14,6 +23,8 @@ public class DateUtils {
 	public final static int minute = 60;
 	public final static int hour = 60;
 	public final static int day = 24;
+
+	public final static String Format_month="yyyy-MM-01 00:00:00";
 	/**
 	 * 获取当前时间
 	 * 
@@ -26,8 +37,18 @@ public class DateUtils {
 	public static String getData(long time) {
 		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(time));
 	}
-	
-	
+
+	public static String getLastMonth(String format){
+		Calendar lCalendar = Calendar.getInstance();
+		lCalendar.add(Calendar.MONTH,-1);
+		return DateFormat.format(format,lCalendar).toString();
+	}
+
+	public static String getCurrent(String format){
+		Calendar lCalendar = Calendar.getInstance();
+		return DateFormat.format(format,lCalendar).toString();
+	}
+
 	/**
 	 * 获取当前时间
 	 * 
