95565eccac036f67e36a77455d3dd473b76516ec
YIslandG <935627337@qq.com>
2016-10-08 16:49:34 +0800

    1.修改getlastmonth -》 getMonth（format,during）

--- base/src/main/java/com/island/gyy/utils/DateUtils.java
+++ base/src/main/java/com/island/gyy/utils/DateUtils.java
@@ -1,12 +1,7 @@
 package com.island.gyy.utils;
 
 
-
-
-
 import android.text.format.DateFormat;
-
-import com.island.gyy.base.BaseApplication;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -14,47 +9,55 @@ import java.util.Date;
 
 /**
  * 时间工具类
- * 
+ *
  * @Author BD
  */
 public class DateUtils {
 
-	public final static int second = 1000;
-	public final static int minute = 60;
-	public final static int hour = 60;
-	public final static int day = 24;
+    public final static int second = 1000;
+    public final static int minute = 60;
+    public final static int hour = 60;
+    public final static int day = 24;
 
-	public final static String Format_month="yyyy-MM-01 00:00:00";
-	/**
-	 * 获取当前时间
-	 * 
-	 * @return
-	 */
-	public static String getData() {
-		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-	}
+    public final static String Format_month = "yyyy-MM-01 00:00:00";
 
-	public static String getData(long time) {
-		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(time));
-	}
+    /**
+     * 获取当前时间
+     *
+     * @return
+     */
+    public static String getData() {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+    }
 
-	public static String getLastMonth(String format){
-		Calendar lCalendar = Calendar.getInstance();
-		lCalendar.add(Calendar.MONTH,-1);
-		return DateFormat.format(format,lCalendar).toString();
-	}
+    public static String getData(long time) {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(time));
+    }
 
-	public static String getCurrent(String format){
-		Calendar lCalendar = Calendar.getInstance();
-		return DateFormat.format(format,lCalendar).toString();
-	}
+    /***
+     * 获取月份，
+     * 
+     * @param format 格式
+     * @param monthDuring 与当前月份的间隔
+     * @return String 返回指定格式化月份时间字符串
+     * **/
+    public static String getMonth(String format, int monthDuring) {
+        Calendar lCalendar = Calendar.getInstance();
+        lCalendar.add(Calendar.MONTH, monthDuring);
+        return DateFormat.format(format, lCalendar).toString();
+    }
 
-	/**
-	 * 获取当前时间
-	 * 
-	 * @return
-	 */
-	public static String getShortData() {
-		return new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-	}
+    public static String getCurrent(String format) {
+        Calendar lCalendar = Calendar.getInstance();
+        return DateFormat.format(format, lCalendar).toString();
+    }
+
+    /**
+     * 获取当前时间
+     *
+     * @return
+     */
+    public static String getShortData() {
+        return new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+    }
 }
